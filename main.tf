378eb158a43ce278317d453f4e6b44e508059c8a
Daniel Allen <dan@aeternagroup.net>
2026-07-08 23:38:26 +0100

    Update to schema version v4.80.0

---
+++ main.tf
@@ -0,0 +1,24 @@
+locals {
+  cdn_endpoints = { for k1, v1 in var.cdn_endpoints : k1 => { content_types_to_compress = v1.content_types_to_compress, delivery_rule = v1.delivery_rule, geo_filter = v1.geo_filter, global_delivery_rule = v1.global_delivery_rule, is_compression_enabled = v1.is_compression_enabled, is_http_allowed = v1.is_http_allowed, is_https_allowed = v1.is_https_allowed, location = v1.location, name = v1.name, optimization_type = v1.optimization_type, origin = v1.origin, origin_host_header = v1.origin_host_header, origin_path = v1.origin_path, probe_path = v1.probe_path, profile_name = v1.profile_name, querystring_caching_behaviour = v1.querystring_caching_behaviour, resource_group_name = v1.resource_group_name, tags = v1.tags } }
+
+  cdn_endpoint_custom_domains = merge([
+    for k1, v1 in var.cdn_endpoints : {
+      for k2, v2 in coalesce(v1.cdn_endpoint_custom_domains, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        cdn_endpoint_id = module.cdn_endpoints.cdn_endpoints["${k1}"].id
+      })
+    }
+  ]...)
+}
+
+module "cdn_endpoints" {
+  source        = "git::https://github.com/AeternaModules/azurerm_cdn_endpoint.git?ref=v4.80.0"
+  cdn_endpoints = local.cdn_endpoints
+}
+
+module "cdn_endpoint_custom_domains" {
+  source                      = "git::https://github.com/AeternaModules/azurerm_cdn_endpoint_custom_domain.git?ref=v4.80.0"
+  cdn_endpoint_custom_domains = local.cdn_endpoint_custom_domains
+  depends_on                  = [module.cdn_endpoints]
+}
+
---
+++ variables.tf
@@ -0,0 +1,231 @@
+variable "cdn_endpoints" {
+  description = <<EOT
+Map of cdn_endpoints, attributes below
+Required:
+    - location
+    - name
+    - profile_name
+    - resource_group_name
+    - origin (block)
+Optional:
+    - content_types_to_compress
+    - is_compression_enabled
+    - is_http_allowed
+    - is_https_allowed
+    - optimization_type
+    - origin_host_header
+    - origin_path
+    - probe_path
+    - querystring_caching_behaviour
+    - tags
+    - delivery_rule (block)
+    - geo_filter (block)
+    - global_delivery_rule (block)
+Nested cdn_endpoint_custom_domains (azurerm_cdn_endpoint_custom_domain):
+    Required:
+        - host_name
+        - name
+    Optional:
+        - cdn_managed_https (block)
+        - user_managed_https (block)
+EOT
+
+  type = map(object({
+    location                      = string
+    name                          = string
+    profile_name                  = string
+    resource_group_name           = string
+    content_types_to_compress     = optional(set(string))
+    is_compression_enabled        = optional(bool)
+    is_http_allowed               = optional(bool) # Default: true
+    is_https_allowed              = optional(bool) # Default: true
+    optimization_type             = optional(string)
+    origin_host_header            = optional(string)
+    origin_path                   = optional(string)
+    probe_path                    = optional(string)
+    querystring_caching_behaviour = optional(string) # Default: "IgnoreQueryString"
+    tags                          = optional(map(string))
+    origin = object({
+      host_name  = string
+      http_port  = optional(number) # Default: 80
+      https_port = optional(number) # Default: 443
+      name       = string
+    })
+    delivery_rule = optional(object({
+      cache_expiration_action = optional(object({
+        behavior = string
+        duration = optional(string)
+      }))
+      cache_key_query_string_action = optional(object({
+        behavior   = string
+        parameters = optional(string)
+      }))
+      cookies_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+        selector         = string
+        transforms       = optional(list(string))
+      }))
+      device_condition = optional(object({
+        match_values     = set(string)
+        negate_condition = optional(bool)   # Default: false
+        operator         = optional(string) # Default: "Equal"
+      }))
+      http_version_condition = optional(object({
+        match_values     = set(string)
+        negate_condition = optional(bool)   # Default: false
+        operator         = optional(string) # Default: "Equal"
+      }))
+      modify_request_header_action = optional(object({
+        action = string
+        name   = string
+        value  = optional(string)
+      }))
+      modify_response_header_action = optional(object({
+        action = string
+        name   = string
+        value  = optional(string)
+      }))
+      name  = string
+      order = number
+      post_arg_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+        selector         = string
+        transforms       = optional(list(string))
+      }))
+      query_string_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+        transforms       = optional(list(string))
+      }))
+      remote_address_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+      }))
+      request_body_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+        transforms       = optional(list(string))
+      }))
+      request_header_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+        selector         = string
+        transforms       = optional(list(string))
+      }))
+      request_method_condition = optional(object({
+        match_values     = set(string)
+        negate_condition = optional(bool)   # Default: false
+        operator         = optional(string) # Default: "Equal"
+      }))
+      request_scheme_condition = optional(object({
+        match_values     = set(string)
+        negate_condition = optional(bool)   # Default: false
+        operator         = optional(string) # Default: "Equal"
+      }))
+      request_uri_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+        transforms       = optional(list(string))
+      }))
+      url_file_extension_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+        transforms       = optional(list(string))
+      }))
+      url_file_name_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+        transforms       = optional(list(string))
+      }))
+      url_path_condition = optional(object({
+        match_values     = optional(set(string))
+        negate_condition = optional(bool) # Default: false
+        operator         = string
+        transforms       = optional(list(string))
+      }))
+      url_redirect_action = optional(object({
+        fragment      = optional(string)
+        hostname      = optional(string)
+        path          = optional(string)
+        protocol      = optional(string) # Default: "MatchRequest"
+        query_string  = optional(string)
+        redirect_type = string
+      }))
+      url_rewrite_action = optional(object({
+        destination             = string
+        preserve_unmatched_path = optional(bool) # Default: true
+        source_pattern          = string
+      }))
+    }))
+    geo_filter = optional(object({
+      action        = string
+      country_codes = list(string)
+      relative_path = string
+    }))
+    global_delivery_rule = optional(object({
+      cache_expiration_action = optional(object({
+        behavior = string
+        duration = optional(string)
+      }))
+      cache_key_query_string_action = optional(object({
+        behavior   = string
+        parameters = optional(string)
+      }))
+      modify_request_header_action = optional(object({
+        action = string
+        name   = string
+        value  = optional(string)
+      }))
+      modify_response_header_action = optional(object({
+        action = string
+        name   = string
+        value  = optional(string)
+      }))
+      url_redirect_action = optional(object({
+        fragment      = optional(string)
+        hostname      = optional(string)
+        path          = optional(string)
+        protocol      = optional(string) # Default: "MatchRequest"
+        query_string  = optional(string)
+        redirect_type = string
+      }))
+      url_rewrite_action = optional(object({
+        destination             = string
+        preserve_unmatched_path = optional(bool) # Default: true
+        source_pattern          = string
+      }))
+    }))
+    cdn_endpoint_custom_domains = optional(map(object({
+      host_name = string
+      name      = string
+      cdn_managed_https = optional(object({
+        certificate_type = string
+        protocol_type    = string
+        tls_version      = optional(string) # Default: "TLS12"
+      }))
+      user_managed_https = optional(object({
+        key_vault_secret_id = string
+        tls_version         = optional(string) # Default: "TLS12"
+      }))
+    })))
+  }))
+
+  validation {
+    condition = alltrue(concat(
+      [for kk in keys(var.cdn_endpoints) : !strcontains(kk, "/")],
+      flatten([for k0, v0 in var.cdn_endpoints : [for kk in keys(coalesce(v0.cdn_endpoint_custom_domains, {})) : !strcontains(kk, "/")]])
+    ))
+    error_message = "Map keys in this package must not contain '/': it is used internally as a nesting-key separator, so a key containing it can silently collide two different nested entries into one. Rename the offending key(s)."
+  }
+}
